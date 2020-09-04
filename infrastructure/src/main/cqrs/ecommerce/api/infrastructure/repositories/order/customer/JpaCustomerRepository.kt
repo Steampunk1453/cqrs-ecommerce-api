c958048309868d8820ae23fbc60dc0d35240bb37
Steampunk <dasies11@gmail.com>
2020-09-04 21:57:06 +0200

    Add new method to save entity

--- infrastructure/src/main/cqrs/ecommerce/api/infrastructure/repositories/order/customer/JpaCustomerRepository.kt
+++ infrastructure/src/main/cqrs/ecommerce/api/infrastructure/repositories/order/customer/JpaCustomerRepository.kt
@@ -3,12 +3,17 @@ package cqrs.ecommerce.api.infrastructure.repositories.order.customer
 import cqrs.ecommerce.api.domain.order.customer.Customer
 import cqrs.ecommerce.api.domain.order.customer.CustomerRepository
 import cqrs.ecommerce.api.infrastructure.entities.order.customer.toDomain
+import cqrs.ecommerce.api.infrastructure.entities.order.customer.toEntity
 import java.util.UUID
 
 class JpaCustomerRepository(private val customerRepository: SpringDataCustomerRepository) : CustomerRepository {
 
     override fun findCustomerById(customerId: UUID): Customer {
         return customerRepository.findById(customerId).orElse(null).toDomain()
+    }
+
+    override fun save(customer: Customer) {
+        customerRepository.save(customer.toEntity())
     }
 
 }
