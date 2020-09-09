fd3f25d758337cf1ce9d645410e751dad98b78d3
Steampunk <dasies11@gmail.com>
2020-09-09 21:26:25 +0200

    Change default value, it's generated after in the handler

--- web/src/main/models/AddressRequest.kt
+++ web/src/main/models/AddressRequest.kt
@@ -2,7 +2,6 @@ package cqrs.ecommerce.api.web.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import cqrs.ecommerce.api.domain.order.customer.Address
-import java.util.UUID
 
 data class AddressRequest(
         @JsonProperty("street") val street: String,
@@ -12,7 +11,7 @@ data class AddressRequest(
 )
 
 fun AddressRequest.toDomain(): Address = Address(
-        id = UUID.randomUUID(),
+        id = null,
         street = street,
         number = number,
         town = town,
