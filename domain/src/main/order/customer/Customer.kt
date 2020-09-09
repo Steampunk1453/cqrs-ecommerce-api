f4a575c514c80661b72c648c5408242dee0efbf2
Steampunk <dasies11@gmail.com>
2020-09-09 21:23:54 +0200

    Allow null values

--- domain/src/main/order/customer/Customer.kt
+++ domain/src/main/order/customer/Customer.kt
@@ -2,4 +2,4 @@ package cqrs.ecommerce.api.domain.order.customer
 
 import java.util.UUID
 
-data class Customer(val id: UUID, val name: String, val address: Address)
+data class Customer(val id: UUID?, val name: String, val address: Address)
