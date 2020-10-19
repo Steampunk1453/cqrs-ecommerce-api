22cc060d055c6135e01cdf2e395318446e6e1149
Steampunk <dasies11@gmail.com>
2020-10-19 20:53:22 +0200

    Remove JsonProperty because it isn't necesary

--- web/src/main/models/AddressRequest.kt
+++ web/src/main/models/AddressRequest.kt
@@ -1,14 +1,13 @@
 package cqrs.ecommerce.api.web.models
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import cqrs.ecommerce.api.domain.order.customer.Address
 import java.util.UUID
 
 data class AddressRequest(
-        @JsonProperty("street") val street: String,
-        @JsonProperty("number") val number: Int,
-        @JsonProperty("town") val town: String,
-        @JsonProperty("country") val country: String
+        val street: String,
+        val number: Int,
+        val town: String,
+        val country: String
 )
 
 fun AddressRequest.toDomain(): Address = Address(
