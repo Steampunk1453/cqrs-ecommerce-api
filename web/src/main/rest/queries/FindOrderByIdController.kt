f4ed17d74f2c8aa62d1f9a5221cb7d73293090fc
Steampunk <dasies11@gmail.com>
2020-10-26 20:51:02 +0100

    Remove future to return dto

--- web/src/main/rest/queries/FindOrderByIdController.kt
+++ web/src/main/rest/queries/FindOrderByIdController.kt
@@ -2,20 +2,21 @@ package cqrs.ecommerce.api.web.rest.queries
 
 import cqrs.ecommerce.api.application.order.queries.FindOrderQuery
 import cqrs.ecommerce.api.domain.order.Order
+import cqrs.ecommerce.api.infrastructure.queries.dtos.OrderDTO
+import cqrs.ecommerce.api.infrastructure.queries.dtos.toDto
 import org.axonframework.queryhandling.QueryGateway
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
-import java.util.concurrent.CompletableFuture
 
 @RestController
 class FindOrderByIdController(val queryGateway: QueryGateway) {
 
     @GetMapping("/orders/{orderId}")
-    fun find(@PathVariable("orderId") orderId: String): CompletableFuture<Order>? {
+    fun find(@PathVariable("orderId") orderId: String): OrderDTO {
         val query = FindOrderQuery(UUID.fromString(orderId))
-        return queryGateway.query(query, Order::class.java)
+        return queryGateway.query(query, Order::class.java).get().toDto()
     }
 
 }
