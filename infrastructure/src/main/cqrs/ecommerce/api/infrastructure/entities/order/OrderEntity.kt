6e523f60dd5243c181f022b0f7544f7e8ba903a1
Steampunk <dasies11@gmail.com>
2020-10-28 23:42:42 +0100

    Add extesion functions for mapping

--- infrastructure/src/main/cqrs/ecommerce/api/infrastructure/entities/order/OrderEntity.kt
+++ infrastructure/src/main/cqrs/ecommerce/api/infrastructure/entities/order/OrderEntity.kt
@@ -5,22 +5,25 @@ import cqrs.ecommerce.api.infrastructure.entities.order.customer.CustomerEntity
 import cqrs.ecommerce.api.infrastructure.entities.order.customer.toDomain
 import cqrs.ecommerce.api.infrastructure.entities.order.customer.toEntity
 import java.util.UUID
+import javax.persistence.CascadeType
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
 @Table(name = "order_table")
 data class OrderEntity(
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        @Id
         val id: UUID,
         @ManyToOne
-        val customer: CustomerEntity
+        val customer: CustomerEntity,
+        @OneToMany(cascade = [(CascadeType.ALL)])
+        val items: List<ItemEntity>,
+        val paid: Boolean
 )
 
-fun Order.toEntity(): OrderEntity = OrderEntity(id, customer.toEntity())
+fun Order.toEntity(): OrderEntity = OrderEntity(id, customer.toEntity(), items().map { it.toEntity() }, paid)
 
 fun OrderEntity.toDomain(): Order =  Order(id, customer.toDomain())
--- infrastructure/src/main/cqrs/ecommerce/api/infrastructure/queries/dtos/FilteredOrderDTO.kt
+++ infrastructure/src/main/cqrs/ecommerce/api/infrastructure/queries/dtos/FilteredOrderDTO.kt
@@ -1,8 +1,14 @@
 package cqrs.ecommerce.api.infrastructure.queries.dtos
 
+import cqrs.ecommerce.api.domain.order.Item
+import cqrs.ecommerce.api.domain.order.Order
 import java.util.UUID
 
 data class OrderDTO(val id: UUID, val items: List<ItemDTO>)
 
 data class ItemDTO(val description: String, val quantity: Int, val value: Double)
+
+fun Order.toDto(): OrderDTO = OrderDTO(id, items().map { it.toDto() })
+
+fun Item.toDto(): ItemDTO = ItemDTO(product.description, 1, product.value.number.toDouble())
 
