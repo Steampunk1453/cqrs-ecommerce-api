ec34d52b577aa258f2ce60cf6a094b680a8fccd9
Steampunk <dasies11@gmail.com>
2020-11-12 14:29:06 +0100

    Get orders filter by customer name and number of orders per customer

---
+++ web/src/main/rest/mappers/OrderPerCustomerMapper.kt
@@ -0,0 +1,13 @@
+package cqrs.ecommerce.api.web.rest.mappers
+
+import cqrs.ecommerce.api.domain.order.Order
+import cqrs.ecommerce.api.web.rest.models.dtos.OrderPerCustomerDTO
+
+class OrderPerCustomerMapper {
+
+    fun map(orders: List<Order>): List<OrderPerCustomerDTO> {
+        return orders.map { OrderPerCustomerDTO(it.customer.name, orders
+                .count { order -> order.customer.id == it.customer.id }) }.distinctBy { it.customer }
+    }
+
+}
---
+++ web/src/main/rest/queries/FindOrdersPerCustomerController.kt
@@ -0,0 +1,22 @@
+package cqrs.ecommerce.api.web.rest.queries
+
+import cqrs.ecommerce.api.application.order.queries.FindOrdersPerCustomerQuery
+import cqrs.ecommerce.api.domain.order.Order
+import cqrs.ecommerce.api.web.rest.mappers.OrderPerCustomerMapper
+import cqrs.ecommerce.api.web.rest.models.dtos.OrderPerCustomerDTO
+import org.axonframework.queryhandling.QueryGateway
+import org.axonframework.queryhandling.responsetypes.ResponseTypes.multipleInstancesOf
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FindOrdersPerCustomerController(val queryGateway: QueryGateway,
+                                      val orderPerCustomerMapper: OrderPerCustomerMapper) {
+
+    @GetMapping("/orders/perCustomer")
+    fun find(): List<OrderPerCustomerDTO>{
+        val orders = queryGateway.query(FindOrdersPerCustomerQuery(""), multipleInstancesOf(Order::class.java)).get()
+        return orderPerCustomerMapper.map(orders)
+    }
+
+}
