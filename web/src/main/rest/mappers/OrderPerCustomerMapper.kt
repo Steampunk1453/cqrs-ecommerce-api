4fbf03bc5215d5f332dca22f9690db9fb6766491
Steampunk <dasies11@gmail.com>
2020-11-12 17:31:39 +0100

    Format code

--- web/src/main/rest/mappers/OrderPerCustomerMapper.kt
+++ web/src/main/rest/mappers/OrderPerCustomerMapper.kt
@@ -6,8 +6,10 @@ import cqrs.ecommerce.api.web.rest.models.dtos.OrderPerCustomerDTO
 class OrderPerCustomerMapper {
 
     fun map(orders: List<Order>): List<OrderPerCustomerDTO> {
-        return orders.map { OrderPerCustomerDTO(it.customer.name, orders
-                .count { order -> order.customer.id == it.customer.id }) }.distinctBy { it.customer }
+        return orders.map {
+            OrderPerCustomerDTO(it.customer.name,
+                    orders.count { order -> order.customer.id == it.customer.id })
+        }.distinctBy { it.customer }
     }
 
 }
