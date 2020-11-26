093efdcd82459c0c4aecdcaba36af96c3b1617c8
Steampunk <dasies11@gmail.com>
2020-11-27 00:01:33 +0100

    Change to command handler

--- application/src/main/cqrs/ecommerce/api/application/order/handlers/OrderPaidHandler.kt
+++ application/src/main/cqrs/ecommerce/api/application/order/handlers/OrderPaidHandler.kt
@@ -1,13 +1,14 @@
-package cqrs.ecommerce.api.application.shipping.eventhandlers
+package cqrs.ecommerce.api.application.order.handlers
 
 import cqrs.ecommerce.api.domain.order.OrderPaid
 import cqrs.ecommerce.api.domain.order.OrderRepository
 import cqrs.ecommerce.api.domain.shipping.ShippingService
+import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventhandling.EventHandler
 
-open class ShipOrderAndNotifyUser(private val orderRepository: OrderRepository,
-                                  private val shippingService: ShippingService) {
-    @EventHandler
+open class OrderPaidHandler(private val orderRepository: OrderRepository,
+                            private val shippingService: ShippingService) {
+    @CommandHandler
     fun handle(event: OrderPaid) {
         val order = orderRepository.findById(event.orderId)
         shippingService.shipOrder(order)
--- application/src/main/cqrs/ecommerce/api/application/order/handlers/PayOrderCommandHandler.kt
+++ application/src/main/cqrs/ecommerce/api/application/order/handlers/PayOrderCommandHandler.kt
@@ -1,20 +1,21 @@
 package cqrs.ecommerce.api.application.order.handlers
 
-import cqrs.ecommerce.api.application.order.commands.PayOrderCommand
 import cqrs.ecommerce.api.domain.order.OrderRepository
+import cqrs.ecommerce.api.domain.order.PayOrderCommand
 import cqrs.ecommerce.api.domain.order.payment.PaymentService
 import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.eventhandling.EventBus
 
 open class PayOrderCommandHandler(private val repository: OrderRepository,
                                   private val paymentService: PaymentService,
-                                  private val eventBus: EventBus) {
+                                  private val commandGateway: CommandGateway) {
 
     @CommandHandler
     fun handle(command: PayOrderCommand) {
         val order = repository.findById(command.orderId)
 
-        order.pay(command.creditCard, paymentService, eventBus) //TODO provide global access to eventBus
+        order.pay(command.creditCard, paymentService, commandGateway)
 
         repository.save(order)
     }
